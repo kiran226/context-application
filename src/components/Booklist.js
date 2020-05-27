a8062ae801b4e9e40d3506f8eb2a49a83fed7e52
kiran <gandikotakiran99@gmail.com>
2020-05-27 13:12:28 +0530

    added navabr and booklist files

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Booklist extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>Bhagavatgita</li>
+                    <li>Ramayanama</li>
+                    <li>Mahabharatam</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Booklist
