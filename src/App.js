3c2bf0020a4efe07f3de966a792c61cae734dbbc
kiran <gandikotakiran99@gmail.com>
2020-05-27 15:59:09 +0530

    created Auth Context

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Navbar from './components/Navbar';
 import Booklist from './components/Booklist';
 import ThemeContextProvider from './context/ThemeContext';
 import ToggleTheme from './components/ThemeToggle';
+import AuthContextProvider from './context/AuthContext';
 
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar/>
-        <Booklist/>
-        <ToggleTheme/>
+        <AuthContextProvider>
+          <Navbar/>
+          <Booklist/>
+          <ToggleTheme/>
+        </AuthContextProvider>
       </ThemeContextProvider> 
     </div>
   );
