67c5b01cc3d026c7518fd400847860660badfb4d
kiran <gandikotakiran99@gmail.com>
2020-05-27 16:10:58 +0530

    consuming multiple contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
+import { AuthContext } from '../context/AuthContext';
 class Navbar extends Component {
     render() {  
         return (
-            <ThemeContext.Consumer>{(context)=>{
-                const {isLightTheme,light,dark}= context;
+            <AuthContext.Consumer>{(authContext)=>(
+                <ThemeContext.Consumer>{(themeContext)=>{
+                const {isLightTheme,light,dark}= themeContext;
+                const {isAuthenticated, toggleAuth}=authContext
                 const theme=isLightTheme?light: dark ;
                 return(
                     <nav style={{background: theme.ui, color:theme.syntax}}>
                         <h1>Context App</h1>
+                        <div onClick={toggleAuth}>
+                            {isAuthenticated? 'Logged in' : 'Logged out'}
+                        </div>
                         <ul>
                             <li>Home</li>
                             <li>About Us</li>
@@ -17,6 +23,8 @@ class Navbar extends Component {
                     </nav>
                 )
             }}</ThemeContext.Consumer>
+            )}</AuthContext.Consumer>
+            
            
         )
     }
