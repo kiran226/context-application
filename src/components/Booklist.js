1a5d4a60094166f6b904354e7953165d0d9a787e
kiran <gandikotakiran99@gmail.com>
2020-06-03 11:05:13 +0530

    added useContext hook to context-api

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import {ThemeContext} from '../context/ThemeContext'
-class Booklist extends Component {
-    static contextType= ThemeContext;
-    render() {
-        const {isLightTheme,light,dark}=this.context;
-        const theme=isLightTheme?light:dark;
-        return (
-            <div className="book-list" style={{color:theme.syntax,background:theme.bg}}>
+
+const BookList = () => {
+    const {isLightTheme,light,dark}=useContext(ThemeContext);
+    const theme=isLightTheme?light:dark;
+    return (  
+        <div className="book-list" style={{color:theme.syntax,background:theme.bg}}>
                 <ul>
                     <li style={{background: theme.ui}}>Bhagavatgita</li>
                     <li style={{background: theme.ui}}>Ramayanama</li>
                     <li style={{background: theme.ui}}>Mahabharatam</li>
                 </ul>
-            </div>
-        )
-    }
+        </div>
+    );
 }
+ 
+export default BookList;
 
-export default Booklist
+
