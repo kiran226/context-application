f2ee6a3b97ff4b34f6d09f9a38803647b7b1dc9c
kiran <gandikotakiran99@gmail.com>
2020-06-03 11:15:02 +0530

    added multiple contexts with useContext hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
 import { AuthContext } from '../context/AuthContext';
-class Navbar extends Component {
-    render() {  
-        return (
-            <AuthContext.Consumer>{(authContext)=>(
-                <ThemeContext.Consumer>{(themeContext)=>{
-                const {isLightTheme,light,dark}= themeContext;
-                const {isAuthenticated, toggleAuth}=authContext
-                const theme=isLightTheme?light: dark ;
-                return(
-                    <nav style={{background: theme.ui, color:theme.syntax}}>
-                        <h1>Context App</h1>
-                        <div onClick={toggleAuth}>
-                            {isAuthenticated? 'Logged in' : 'Logged out'}
-                        </div>
-                        <ul>
-                            <li>Home</li>
-                            <li>About Us</li>
-                            <li>Contact</li>
-                        </ul>
-                    </nav>
-                )
-            }}</ThemeContext.Consumer>
-            )}</AuthContext.Consumer>
-            
-           
-        )
-    }
-}
 
-export default Navbar
+const Navbar = () => {
+    const {isLightTheme,light,dark} =useContext(ThemeContext);
+    const {isAuthenticated, toggleAuth}= useContext(AuthContext);
+    const theme=isLightTheme?light: dark ;
+    return ( 
+        <nav style={{background: theme.ui, color:theme.syntax}}>
+            <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                {isAuthenticated? 'Logged in' : 'Logged out'}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact</li>
+            </ul>
+         </nav>
+     );
+}
+ 
+export default Navbar;
+
